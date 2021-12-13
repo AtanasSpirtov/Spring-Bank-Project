cbf6c181de4b8bcdce7564742cbe5ee69d3c8696
spirtov <spirtovv@Gmail.com>
2021-12-13 11:43:49 +0200

    Implemented method findByName

--- src/main/java/com/example/demo/service/AccountServiceImpl.java
+++ src/main/java/com/example/demo/service/AccountServiceImpl.java
@@ -6,7 +6,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
-import javax.persistence.LockModeType;
 import java.math.BigDecimal;
 
 import static javax.persistence.LockModeType.PESSIMISTIC_WRITE;
@@ -25,12 +24,10 @@ public class AccountServiceImpl extends _BaseService implements AccountService {
     public Account deleteAccount(String name) {
         Account account = em.createQuery("select a from Account a where a.name =: pName" , Account.class)
                 .setParameter("pName" , name)
-                .setLockMode(PESSIMISTIC_WRITE)
                 .getSingleResult();
 
         em.createQuery("delete from Account a where a.name=: pName")
                 .setParameter("pName" , name)
-                .setLockMode(PESSIMISTIC_WRITE)
                 .executeUpdate();
         return account;
     }
@@ -43,5 +40,12 @@ public class AccountServiceImpl extends _BaseService implements AccountService {
     @Override
     public EntityManager getEntityManager() {
         return super.getEntityManager();
+    }
+
+    @Override
+    public Account findByName(String accountName) {
+        return em.createQuery("select a from Account a where a.name=:pName " , Account.class)
+                .setParameter("pName" , accountName)
+                .getSingleResult();
     }
 }
