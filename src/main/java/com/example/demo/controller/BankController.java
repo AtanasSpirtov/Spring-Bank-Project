3a5f19b244a172c2c4df50a031b0eb6d51a85d78
spirtov <spirtovv@Gmail.com>
2021-12-13 11:47:49 +0200

    fixed mapping and changed parameters for better communication with frontend

--- src/main/java/com/example/demo/controller/BankController.java
+++ src/main/java/com/example/demo/controller/BankController.java
@@ -2,15 +2,14 @@ package com.example.demo.controller;
 
 import com.example.demo.controller.dto.TransactionDTO;
 import com.example.demo.controller.dto.MessageDTO;
-import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
+import com.example.demo.model.Transaction;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-import java.math.BigDecimal;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -19,18 +18,19 @@ import java.util.stream.Collectors;
 public class BankController extends _BaseController {
 
 
-    @GetMapping("/create")
-    public ResponseEntity<MessageDTO> create(@RequestParam Long accountFromId, @RequestParam Long accountToId
-            , @RequestParam BigDecimal amount) {
+    @PostMapping(value = "/create",
+            consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE}
+    )
+    public ResponseEntity<MessageDTO> create(@RequestBody Transaction transaction) {
         //Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         logger.info("Entering create ...");
-        transactionService.transferMoney(accountFromId, accountToId, amount);
+        transactionService.transferMoney(transaction.getSourceAccount().getId(), transaction.getRecipientAccount().getId(), transaction.getTransactionAmount());
         return new ResponseEntity<>(new MessageDTO("OK"), HttpStatus.OK);
     }
 
-    @PostMapping("/allTransactionsByAccount")
+    @GetMapping("/allTransactionsByAccount/{accountId}")
     public @ResponseBody
-    ResponseEntity<List<TransactionDTO>> allTransactionsByAccount(@RequestParam Long accountId) {
+    ResponseEntity<List<TransactionDTO>> allTransactionsByAccount(@PathVariable("accountId") Long accountId) {
         HttpHeaders headers = new HttpHeaders();
 
         logger.info("Entering list all transactions ...");
