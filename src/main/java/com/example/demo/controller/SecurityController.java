5a4a91cc2c2a8e123e9c603f3fca8b60060a0e55
spirtov <spirtovv@Gmail.com>
2022-02-03 11:23:12 +0200

    changed security controller

--- src/main/java/com/example/demo/controller/SecurityController.java
+++ src/main/java/com/example/demo/controller/SecurityController.java
@@ -1,23 +1,16 @@
 package com.example.demo.controller;
 
 import com.example.demo.controller.dto.MessageDTO;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
-@RequestMapping("/user")
 public class SecurityController extends _BaseController{
-    @GetMapping("/signUp")
-    public ResponseEntity<MessageDTO> signIn(@RequestParam String username
-            ,@RequestParam String password ,@RequestParam String email) {
-        logger.info("Creating user.....");
-        //authenticationService.register(username , password , email);
-        return new ResponseEntity<>(new MessageDTO("User Successfully Created") , HttpStatus.OK);
+    @GetMapping("/login")
+    @ResponseBody public ResponseEntity<MessageDTO> login() {
+        logger.info("Entering login ...");
+        return new ResponseEntity<>(new MessageDTO("Authenticated Successfully"), HttpStatus.OK);
     }
-
 }
