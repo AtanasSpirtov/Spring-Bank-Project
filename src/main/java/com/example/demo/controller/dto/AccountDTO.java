0a1f34165c6b6e24a16835ddc406edb8808cef3a
spirtov <spirtovv@Gmail.com>
2022-02-03 11:25:16 +0200

    added ID to accountDTO

--- src/main/java/com/example/demo/controller/dto/AccountDTO.java
+++ src/main/java/com/example/demo/controller/dto/AccountDTO.java
@@ -3,10 +3,15 @@ package com.example.demo.controller.dto;
 import java.math.BigDecimal;
 
 public class AccountDTO {
+    private Long id;
 
     private String name;
 
     private BigDecimal balance;
+
+    public Long getId() {return id;}
+
+    public void setId(Long id) {this.id = id;}
 
     public String getName() {
         return name;
